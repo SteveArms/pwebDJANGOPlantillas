6753c9a7cb9cb8873f5865a1445ab146b8a5444f
SteveArms <acunoc@unsa.edu.pe>
2024-06-05 11:06:07 -0500

    Login en Django

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -25,9 +25,24 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
-                return redirect('/')
+                return redirect('login')
         else:
             messages.info(request ,'password not matching...')
             return redirect('register')
     else:
         return render(request, 'register.html')
+    
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'innvalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
