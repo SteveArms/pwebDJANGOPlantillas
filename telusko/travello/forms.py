afed506c39104ad36d56244acf82b9e20345263e
SteveArms <acunoc@unsa.edu.pe>
2024-06-05 21:30:03 -0500

    CSS para el agregar destino

--- telusko/travello/forms.py
+++ telusko/travello/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import Destination
 from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Field
 
 class DestinationForm(forms.ModelForm):
     class Meta:
